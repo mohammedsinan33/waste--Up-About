d7d4fdf734f58e814e463cb59d7bbb7f1acf951a
Mohammed Sinan K <mohammedsinan.k.33@gmail.com>
2023-10-08 04:17:52 +0530

    About

--- src/App.js
+++ src/App.js
@@ -2,24 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div className="main">
+    <div className="header">
+      <div className="left">
+        <p className="p1">10000+</p>
+        <p className="p2">WASTE COLLECTED</p>
+      </div>
+      <div className="middle">
+        <p className="p1">1000+</p>
+        <p className="p2">INFORMATIONS</p>
+      </div>
+      <div className="right">
+        <p className="p1">500+</p>
+        <p className="p23">REWARDED</p>
+      </div>
     </div>
+    <div className="middle2">
+      <h2>Do You ever feel shy to respond or feel like you couldn't do anything for the society?</h2>
+    </div>
+    <div className="footer">
+      <div className="left2">
+        <div className="left2_upper">
+          <div className="Arjun">
+            <p>See Arjun, how much waste is on that road?</p>
+          </div>
+        </div>
+        <div className="left2_middle">
+          <div className="left2_middle_left">
+            <img src="3.png" alt="" />
+          </div>
+          <div className="left2_middle_right">
+            <div className="Arjun1">
+              <p>See Arjun, how much waste is on that road?</p>
+            </div>
+          </div>
+        </div>
+        <div className="left2_footer">
+          <div className="Arjun2">
+            <p>See Arjun, how much waste is on that road?</p>
+          </div>
+        </div>
+      </div>
+      <div className="right2">
+        <div className="right2_main">
+          <img src="9.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+
   );
 }
-
 export default App;
